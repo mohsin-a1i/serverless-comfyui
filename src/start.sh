4e7c9d421ac2147d387235b48057f48eadfb17d2
mohsin-a1i <mohsin_ali96@hotmail.com>
2026-01-20 01:23:29 +0500

    Removed comfy manager offline script

--- src/start.sh
+++ src/start.sh
@@ -4,9 +4,6 @@
 TCMALLOC="$(ldconfig -p | grep -Po "libtcmalloc.so.\d" | head -n 1)"
 export LD_PRELOAD="${TCMALLOC}"
 
-# Ensure ComfyUI-Manager runs in offline network mode inside the container
-comfy-manager-set-mode offline || echo "worker-comfyui - Could not set ComfyUI-Manager network_mode" >&2
-
 echo "worker-comfyui: Starting ComfyUI"
 
 # Allow operators to tweak verbosity; default is INFO.
